23f3c42cad04f56b55c911a3b32034935284b45c
kaitlyn_m <kaitlyn.a.marino@gmail.com>
2021-06-07 15:07:47 -0400

    Updated header to meet standard testing codes

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
 
 <head>
   <meta charset="utf-8">
@@ -19,24 +19,16 @@
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/normalize.css">
 
+  <script src="js/main.js" defer></script>
+
   <meta name="theme-color" content="#fafafa">
 
-  <SCRIPT>
-    function ShowAndHide() {
-        var x = document.getElementById('mobile-navigation');
-        if (x.style.display == 'none') {
-            x.style.display = 'flex';
-        } else {
-            x.style.display = 'none';
-        }
-    }
-</SCRIPT>
 
 </head>
 
 <body>
-  <header> <!--Logo, Search, Navigation Menu & Hero Image-->
-    <section id="top-banner"> <!--Logo, Search Icon (Java to pop out text box), Navigation Menu-->
+<header> <!--Logo, Search, Navigation Menu & Hero Image-->
+  <div class="top-banner"> <!--Logo, Search Icon (Java to pop out text box), Navigation Menu-->
       
       <div class="half-width-left"> <!--Needs to be set in half width divs to align content correctly-->
         <div class="logo">
@@ -47,39 +39,21 @@
       </div>
 
       <div class="half-width-right">
-
         <div class="search">
           <img src="../img/magnify-glass.png" alt="Search Icon" />
         </div>
+        <nav>
+          <div class="mobile-nav">
+            <div class="hamburger">
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+            </div>
+          </div>
+        </nav>
 
-        <div class="hamburger">
-          <a href="#" class="mobile-nav">
-            <BUTTON ONCLICK="ShowAndHide()">
-                <div class="line"></div>
-                <div class="line"></div>
-                <div class="line"></div>
-            </BUTTON>
-          </a>
-          <nav>
-            <ul class="mobile-nav-links">
-              <li><a href="../login/index.php">Login</a></li>
-              <li><a href="../about-us/index.php">About Us</a></li>
-              <li><a href="../dental-products/index.php">Dental</a></li>
-              <li><a href="../vision-products/index.php">Vision</a></li>
-              <li><a href="https://dominiondentists.com/dominion" target="_blank">Find a Dentist</a></li>
-              <li><a href="../find-a-vision-provider/index.php">Find a Vision Provider</a></li>
-              <li><a href="../health-facts/index.php">Health Facts</a></li>
-              <li><a href="../news/index.php">News</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    
-    </section>
-
-    <section id="mobile-navigation" STYLE="display:none"> <!--Needs seperate section for mobile nav dropdown due to javascript-->
-      <nav>
-        <ul class="mobile-nav-links">
+        <div class="mobile-nav-links hide">
+          <ul>
             <li><a href="../login/index.php">Login</a></li>
             <li><a href="../about-us/index.php">About Us</a></li>
             <li><a href="../dental-products/index.php">Dental</a></li>
@@ -88,14 +62,15 @@
             <li><a href="../find-a-vision-provider/index.php">Find a Vision Provider</a></li>
             <li><a href="../health-facts/index.php">Health Facts</a></li>
             <li><a href="../news/index.php">News</a></li>
-        </ul>
-      </nav>
-    </section>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
 
-
-    <section id="hero-container">
-      <img class="hero" src="../img/hero-image-tablet.png" height="300px" />
-    </section>
+    <div class="hero-container">
+      <img class="hero" src="../img/hero-image-tablet.png" alt="Hero Image" />
+    </div>
 
   </header>
 
