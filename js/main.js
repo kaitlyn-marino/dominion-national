20b281a26e294a9656acfac69412bfc8d9aee6b9
kaitlyn_m <kaitlyn.a.marino@gmail.com>
2021-08-05 18:12:50 -0400

    Fixed mobile nav menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const menuButton = document.querySelector(".hamburger");
 const menuNav = document.querySelector(".mobile-nav-links");
+const rightHalf = document.querySelector(".half-width-right");
+const leftHalf = document.querySelector(".half-width-left");
+const searchIcon = document.querySelector(".search");
+const logo = document.querySelector(".logo");
 
 menuButton.addEventListener("click", function () {
     menuNav.classList.remove("hide");
     menuButton.classList.add("hide");
+    rightHalf.style.width = "100%";
+    leftHalf.style.width = "100%";
+    searchIcon.classList.add("hide");
+    leftHalf.style.justifyContent = "center";
 });
